fcf5d0ef023337df744e4c8e018668dd82f67e23
s0s01qp <shubham.sinha@walmartlabs.com>
2021-07-25 17:06:10 +0530

    added test cases

--- flags/flags_test.go
+++ flags/flags_test.go
@@ -7,10 +7,22 @@ import (
 	"testing"
 )
 
+func TestCertFilePath(t *testing.T) {
+	assert.Equal(t, commons.CertFilePathDefaultValue, flags.CertFilePath())
+}
+
+func TestKeyFilePath(t *testing.T) {
+	assert.Equal(t, commons.KeyFilePathDefaultValue, flags.KeyFilePath())
+}
+
 func TestPersistencePath(t *testing.T) {
 	assert.Equal(t, commons.PersistenceDefaultValue, flags.PersistencePath())
 }
 
 func TestPort(t *testing.T) {
 	assert.Equal(t, commons.PortDefaultValue, flags.Port())
+}
+
+func TestTLSEnabled(t *testing.T) {
+	assert.Equal(t, commons.TLSEnabledDefaultValue, flags.TLSEnabled())
 }
